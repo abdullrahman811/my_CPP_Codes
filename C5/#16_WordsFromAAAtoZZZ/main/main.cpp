73f85aaa7bf69a2856dd5569188b6a62944bb999
abdullrahman811 <abdullrahmandawaliby1@gmail.com>
2026-03-13 02:11:55 +0300

    C5/16: string of word

--- C5/#16_WordsFromAAAtoZZZ/main/main.cpp
+++ C5/#16_WordsFromAAAtoZZZ/main/main.cpp
@@ -3,13 +3,22 @@ using namespace std;
 
 void printWordsFromAAAtoZZZ()
 {
+    string word = "";
+
     for (int i = 'A'; i <= 'Z'; i++)
     {
         for (int j = 'A'; j <= 'Z'; j++)
         {
             for (int k = 'A'; k <= 'Z'; k++)
             {
-                cout << char(i) << char(j) << char(k);
+                word += char(i);
+                word += char(j);
+                word += char(k);
+
+                cout << word;
+
+                word = "";
+                
                 cout << "\n";
             }
 
