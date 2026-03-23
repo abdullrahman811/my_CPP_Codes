b89b6608b0aa8d70341739c4ef328ca7473a6c37
abdullrahman811 <abdullrahmandawaliby1@gmail.com>
2026-03-23 03:40:54 +0300

    feat: C5:L26 (neovim termux)

---
+++ C5/#26_fillRandomArrayAndSum/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>  
+using namespace std;  
+int readPositiveNumber(string message) 
+{   
+  int num = 0;   
+  do   
+  {    
+    cout << message << endl;     
+    cin >> num;
+    } while (num <= 0);  
+    
+    return num;
+}  
+
+int randomNumber(int From, int To) {
+  int randNum = rand() % (To - From + 1) + From;
+  return randNum;
+}
+
+void randomArray(int array[100], int &arraySize)
+{ 
+  arraySize = readPositiveNumber("\nEnter Array Elements Number: ");    
+
+  for (int i = 0; i < arraySize; i++)  
+  {     
+    array[i] = randomNumber(1, 100);   
+  }
+}  
+
+void printArray(int array[100], int arraySize)
+{   
+  for (int i = 0; i < arraySize; i++) 
+  {       
+    cout << array[i] << " ";  
+  }  
+} 
+
+int sumOfArray(int array[100], int arraySize)
+{
+  int sum = 0;
+    for (int i = 0; i < arraySize; i++)
+    {
+      sum += array[i];
+    }
+
+  return sum;
+}
+
+
+void orchestrator()
+{   
+  int array[100];
+  int arraySize = 0;
+
+  randomArray(array, arraySize);
+  printArray(array, arraySize);
+
+  cout << "\nThe Sum Of The Array Is: " << sumOfArray(array, arraySize);
+}
+
+int main() 
+{   
+  srand((unsigned)time(NULL));   
+  orchestrator();   
+  return 0;
+}
