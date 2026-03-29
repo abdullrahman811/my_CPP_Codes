3ddb809070fc8af295f6dfd7d608ffa5af9f08af
abdullrahman811 <abdullrahmandawaliby1@gmail.com>
2026-03-29 10:40:45 +0300

    feat: C5:L36

---
+++ C5/#36_AddArrayElementsSemiDynamically/main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+using namespace std;
+
+int readPositiveNumber(string message)
+{
+    int num = 0;
+
+    do
+    {
+        cout << message << endl;
+        cin >> num;
+    } while (num <= 0);
+
+    return num;
+}
+
+int readNumber(string message)
+{
+    int num = 0;
+
+    cout << message << endl;
+    cin >> num;
+    
+    return num;
+}
+
+
+bool readBool(string message)
+{
+    bool boolean = false;
+
+    cout << message;
+    cin >> boolean;
+
+    return boolean;
+}
+
+/*void readArray(int array[100], int arraySize)
+{
+    for (int i = 0; i < arraySize; i++)
+    {
+        cout << "\n Element [" << i + 1 << "]: ";
+        cin >> array[i];
+    }
+}*/
+
+void semiDynamicArraySize(int array[100], int &arraySize)
+{
+    int counter = 0;
+    bool check = 0;
+    do
+    {
+        counter++;
+        array[counter - 1] = readNumber("\nPlease Enter Number: ");
+        check = readBool("Do You Want To Add More Numbers? (0 For No, 1 For Yes) : ");
+    } while (check);
+    
+    arraySize = counter;
+}
+
+void printArray(int array[100], int arraySize)
+{
+    
+    for (int i = 0; i < arraySize; i++)
+    {
+        cout << array[i] << " ";
+    }
+    
+}
+
+void maestro()
+{
+    int array[100];
+    int arraySize = 0;
+    
+    semiDynamicArraySize(array, arraySize);
+
+    cout << "\n\n\nArray Length: " << arraySize;
+
+    cout << "\nArray Elements: ";
+    printArray(array, arraySize);
+}
+
+int main()
+{
+    maestro();
+
+    return 0;
+}
