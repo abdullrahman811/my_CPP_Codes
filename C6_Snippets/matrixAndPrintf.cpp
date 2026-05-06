be49e097d1ff53c16cb28ec75a6f411155e72676
it008e <abdullrahmandawaliby1@gmail.com>
2026-05-07 00:14:55 +0300

    Add: Multiplication Table V2

---
+++ C6_Snippets/matrixAndPrintf.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void fillTable(int table[10][10])
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    for (int n : arr)
+    {
+        for (int m : arr)
+        {
+            table[n - 1][m - 1] = n * m;
+        }
+    }
+}
+
+void printTable(int table[10][10])
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    for (int n : arr)
+    {
+        for (int m : arr)
+        {
+            setw(3);
+            printf(" %0*d", 2, table[n - 1][m - 1]);
+        }
+
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int multiplicationTable[10][10];
+
+    fillTable(multiplicationTable);
+    printTable(multiplicationTable);
+
+    return 0;
+}
