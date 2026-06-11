836d448d771ea13b20a59de142fb196ce749802b
it008e <abdullrahmandawaliby1@gmail.com>
2026-06-11 13:51:44 +0000

    Add: C7:L7

---
+++ C7/#7_Transpose_Matrix/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void printMatrix(int matrix[3][3]);
+
+void printTransposedMatrix(int matrix[3][3]);
+
+void fillMatrixOrderedNum(int matrix[3][3]);
+
+int main() {
+    
+    int matrix[3][3];
+    
+    fillMatrixOrderedNum(matrix);
+    
+    printMatrix(matrix);
+    
+    cout << "\nTransposed Matrix: \n";
+    
+    printTransposedMatrix(matrix);
+    return 0;
+}
+
+void printMatrix(int matrix[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << setw(3) << matrix[i][j] << "\t";
+        }
+        
+        cout << "\n";
+    }
+}
+
+void fillMatrixOrderedNum(int matrix[3][3])
+{
+    int val = 1;
+    
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+              matrix[i][j] = val++;
+        }
+    }
+}
+
+void printTransposedMatrix(int matrix[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << setw(3) << matrix[j][i] << "\t";
+        }
+        
+        cout << "\n";
+    }
+}
