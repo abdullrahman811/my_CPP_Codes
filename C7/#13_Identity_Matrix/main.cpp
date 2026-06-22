31bbfb117dbd66f82df088cf621c9b35fc6f19f8
it008e <abdullrahmandawaliby1@gmail.com>
2026-06-22 12:50:39 +0000

    Add: C7:L13

---
+++ C7/#13_Identity_Matrix/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include "../../lib/all.hpp"
+
+using namespace std;
+using namespace all;
+
+void fillMatrixInRandoms(int matrix[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            matrix[i][j] = randomNumber(1, 10);
+        }   
+    }
+}
+
+void printMatrix(int matrix[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << setw(3) << matrix[i][j] << "\t";
+        }
+        
+        cout << "\n";
+    }
+}
+
+bool isIdentityMatrix(int mat[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (i == j)
+            {
+                if (mat[i][j] != 1)
+                    return false;
+            }
+            
+            else if (mat[i][j] != 0)
+                return false;
+        }
+    }
+    
+    return true;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int matrix[3][3] = { 
+      {1, 0, 0},
+      {0, 1, 0},
+      {0, 0, 1}
+     };
+
+    //fillMatrixInRandoms(matrix);
+
+    printMatrix(matrix);
+    
+    cout << "\n" << isIdentityMatrix(matrix);
+
+    return 0;
+}
