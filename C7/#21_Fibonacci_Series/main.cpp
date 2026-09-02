c7e41081571ec126e455b8c19aa4c7c57f664a30
Abdullrahman <abdullrahmandawaliby1@gmail.com>
2026-09-02 23:32:55 +0300

    Refractor: Fixed Indentation

--- C7/#21_Fibonacci_Series/main.cpp
+++ C7/#21_Fibonacci_Series/main.cpp
@@ -6,23 +6,24 @@ using namespace all;
 
 void printFibonacci(int num)
 {
-				int prev2 = 0, prev1 = 1, fibNum = 0;
-				//cout << "\n" << prev2 << prev1;
-				
-				for (int i = 1; i <= num; i++) {
-								prev2 = prev1;
-								prev1 = fibNum;
-								fibNum = prev1 + prev2;
-								
-								cout << fibNum << "\t";
-				}
+	int prev2 = 0, prev1 = 1, fibNum = 0;
+	// cout << "\n" << prev2 << prev1;
+
+	for (int i = 1; i <= num; i++)
+	{
+		prev2 = prev1;
+		prev1 = fibNum;
+		fibNum = prev1 + prev2;
+
+		cout << fibNum << "\t";
+	}
 }
 
 int main()
 {
-				int num = readPositiveNumber("\nEnter A Positive Number: ");
-				
-				printFibonacci(num);
-				
-				return 0;
+	int num = readPositiveNumber("\nEnter A Positive Number: ");
+
+	printFibonacci(num);
+
+	return 0;
 }
