a9ac2c4b79a229d78a079baf57494e674a70de3b
it008e <abdullrahmandawaliby1@gmail.com>
2026-09-26 18:50:46 +0300

    add: joinString Array

---
+++ C7/#40_Join_String_Array/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "../../lib/it.hpp"
+
+std::string joinString(const std::string words[], unsigned int arrLength, const std::string &delim = " ")
+{
+    std::string mergedText;
+    bool isFirst = true;
+
+    for (unsigned int i = 0; i < arrLength; i++)
+    {
+        if (!isFirst)
+            mergedText += delim
+        ;
+        
+        mergedText += words[i];
+    
+        isFirst = false;        
+    }
+
+    return mergedText;
+}
+
+int main()
+{
+    std::string words[] = {"hi", "how", "are", "you"};
+
+    std::cout << it008e::Strings::toTitleCase("array after join: ") << joinString(words, 4, " ");
+
+    return 0;
+}
