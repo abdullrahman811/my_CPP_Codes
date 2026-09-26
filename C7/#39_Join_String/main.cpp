8ba8fca182a3e2237bc3c73532f130614684f1c3
it008e <abdullrahmandawaliby1@gmail.com>
2026-09-26 18:34:20 +0300

    add: joinString

---
+++ C7/#39_Join_String/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "../../lib/it.hpp"
+
+std::string joinString(const std::vector <std::string> &vWords, const std::string &delim = " ")
+{
+    std::string mergedText;
+    bool isFirst = true;
+
+    for (const std::string & s: vWords)
+    {
+        if (!isFirst)
+            mergedText += delim
+        ;
+        
+        mergedText += s;
+
+        isFirst = false;
+    }
+
+    return mergedText;
+}
+
+int main()
+{
+    std::vector <std::string> vWords = {"hi", "how", "are", "you"};
+
+    std::cout << it008e::Strings::toTitleCase("vector after join: ") << joinString(vWords);
+
+    return 0;
+}
